c838b5ab1c42d50ae5684d9252f431ab291ec50e
xydiva <xydiva1@gmail.com>
2015-09-24 17:19:27 +0800

    更新到正式数据库；

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var routes = require('./routes/index');
 var todos = require('./routes/todos');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/todoApp', function (err) {
+mongoose.connect('mongodb://diva:114121@ds031932.mongolab.com:31932/heroku_jc43hpnq', function (err) {
     if (err) {
         console.log('connection error', err);
     } else {
